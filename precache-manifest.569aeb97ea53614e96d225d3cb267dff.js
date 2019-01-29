5857049d230166ec48e94436355a707bc685056a
Bohdan Tsybulskyi <b.tsybulskyy@gmail.com>
2019-01-29 18:14:29 +0200

    Updates

---
+++ precache-manifest.569aeb97ea53614e96d225d3cb267dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbaeda476c1bb181a354",
+    "url": "/testcoax/static/js/runtime~main.cbaeda47.js"
+  },
+  {
+    "revision": "184056bb1508578760b4",
+    "url": "/testcoax/static/js/main.184056bb.chunk.js"
+  },
+  {
+    "revision": "e8e68331b904bb341a51",
+    "url": "/testcoax/static/js/1.e8e68331.chunk.js"
+  },
+  {
+    "revision": "184056bb1508578760b4",
+    "url": "/testcoax/static/css/main.6b66a70a.chunk.css"
+  },
+  {
+    "revision": "bcb50b1f4bd1c46293fc6e2a282e0273",
+    "url": "/testcoax/index.html"
+  }
+];
